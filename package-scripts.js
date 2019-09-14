ce0dd84d9031978365477057e6243c0db5dcbcfb
Oliver Caine <olliecaine@gmail.com>
2019-09-15 01:40:05 +1000

    Reverting to yarn

--- package-scripts.js
+++ package-scripts.js
@@ -36,7 +36,7 @@ module.exports = {
          */
         setup: {
             script: series(
-                'npm install',
+                'yarn install',
                 'nps db.setup',
             ),
             description: 'Setup`s the development environment(yarn & database)'
